81a4c097ff931868d423c553fe897ca86bd8995f
Peter Gilbert <petegilb@bu.edu>
2020-12-12 18:33:00 -0500

    little pug update

--- prototype/routes/index.js
+++ prototype/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const fetch = require('node-fetch');
 const fetchConfig = require('../config/fetchConfig');
 const tmdbConfig = require('../config/tmdbConfig');
