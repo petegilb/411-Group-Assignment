b30b1d48585eb50d2a8d3235235e490ac1c35a8b
Peter Gilbert <petegilb@bu.edu>
2020-12-12 15:55:58 -0500

    basic functionality using json file

--- prototype/routes/index.js
+++ prototype/routes/index.js
@@ -10,14 +10,16 @@ router.route('/')
         res.render('index', {title: 'Movie Recommendations'});
     })
     .post(async (req, res, next) => {
+        const movieJson = require('../movies.json');
         let genre = (req.body.MovieGenre ? req.body.MovieGenre: 'Action');
-        res.render('index', {results: "you chose " + genre});
-        /*let input;
-        if (req.body.movieTitle) {
-            input = req.body.movieTitle;
-        } else
-            input = "Star Wars";
-        let rawReturnValue = await fetch(fetchConfig.fetchOptions.url + "?apikey=" + fetchConfig.fetchOptions.key + "&t=" + input, {method: fetchConfig.fetchOptions.method});
+        let movies = movieJson["movies"];
+        //res.render('index', {results: "you chose " + genre});
+        //console.log(movieJson["movies"][0]["title"]);
+
+        //find random movie matching the genre
+        let movieTitle = await findMovie(genre, movies);
+        //now find the information off of the movie title -> eventually we will want to store this in a database
+        let rawReturnValue = await fetch(fetchConfig.fetchOptions.url + "?apikey=" + fetchConfig.fetchOptions.key + "&t=" + movieTitle, {method: fetchConfig.fetchOptions.method});
         const cleanReturnValue = await rawReturnValue.json();
         //res.render('index', {results: fetchConfig.fetchOptions.url + "?apikey=" + fetchConfig.fetchOptions.key + "&t=" + input});
         try {
@@ -35,8 +37,13 @@ router.route('/')
             });
         } catch (error) {
             res.render('index', {results: "unable to find movie"});
-        }*/
+        }
     })
 
+//function to find a random movie from the list
+const findMovie = async (genre, movies) => {
+    let filteredMov = movies.filter(movie => movie["genres"].includes(genre));
+    return(filteredMov[Math.floor(Math.random() * filteredMov.length)]["title"]);
+}
 
 module.exports = router;
