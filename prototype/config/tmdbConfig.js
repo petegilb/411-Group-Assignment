4f103f868b2841d6c5d2ce33b0ea5f3346955d69
Peter Gilbert <petegilb@bu.edu>
2020-12-15 12:42:04 -0500

    removed config

--- prototype/config/tmdbConfig.js
+++
@@ -1,8 +0,0 @@
-//config for our api
-const tmdbOptions = {
-    method: 'GET',
-    url: 'https://api.themoviedb.org/3/',
-    key: '99ed7f2e4180976d92e07f1b7b0c6c9a'
-};
-
-module.exports = {tmdbOptions};
